10d03a97b942823437d046b2e92f0ba1fe3f2f26
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-03-13 14:12:46 +0000

    05- split

---
+++ 05/split.js
@@ -0,0 +1,24 @@
+//Declare a varibale and assign string value to it and then split it into an array using split() method
+
+// String
+let str = "I'm String"
+console.log("This is String -");
+console.log(str);
+
+// split()
+let split = str.split()
+console.log("This is String inside Array -");
+console.log(split);
+
+// split (seperator)
+
+let sep = str.split(" ")
+console.log("string sepreted inside Array -");
+console.log(sep);
+
+// split (seperator, limt) 
+let sepe = "Hello how are you"
+let sepLimit = sepe.split(" ", 2)
+console.log("string sepreted with limit inside Array -");
+
+console.log(sepLimit);
